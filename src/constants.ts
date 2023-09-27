7dc8da034a2eba32d273a7e64fa35dca0579ef02
Geoffray <46541094+Jofresh@users.noreply.github.com>
2023-09-27 13:41:00 +0200

    feat: ajouter les blocs qui bloquent (#8) (#14)
    * feat: add bloc image (#8)
    
    * feat: add wall/bloc component (#8)
    
    * feat: add bloc coords (#8)
    
    * feat: add bloc in game (#8)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,46 @@
 export const GRID_SIZE = 13;
 export const TILE_SIZE = 32;
 export const MAP_SIZE = GRID_SIZE * TILE_SIZE;
+export const WALLS = [
+    { x: TILE_SIZE * 1, y: TILE_SIZE },
+    { x: TILE_SIZE * 3, y: TILE_SIZE },
+    { x: TILE_SIZE * 5, y: TILE_SIZE },
+    { x: TILE_SIZE * 7, y: TILE_SIZE },
+    { x: TILE_SIZE * 9, y: TILE_SIZE },
+    { x: TILE_SIZE * 11, y: TILE_SIZE },
+    
+    { x: TILE_SIZE * 1, y: TILE_SIZE * 3 },
+    { x: TILE_SIZE * 3, y: TILE_SIZE * 3 },
+    { x: TILE_SIZE * 5, y: TILE_SIZE * 3 },
+    { x: TILE_SIZE * 7, y: TILE_SIZE * 3 },
+    { x: TILE_SIZE * 9, y: TILE_SIZE * 3 },
+    { x: TILE_SIZE * 11, y: TILE_SIZE * 3 },
+
+    { x: TILE_SIZE * 1, y: TILE_SIZE * 5 },
+    { x: TILE_SIZE * 3, y: TILE_SIZE * 5 },
+    { x: TILE_SIZE * 5, y: TILE_SIZE * 5 },
+    { x: TILE_SIZE * 7, y: TILE_SIZE * 5 },
+    { x: TILE_SIZE * 9, y: TILE_SIZE * 5 },
+    { x: TILE_SIZE * 11, y: TILE_SIZE * 5 },
+
+    { x: TILE_SIZE * 1, y: TILE_SIZE * 7 },
+    { x: TILE_SIZE * 3, y: TILE_SIZE * 7 },
+    { x: TILE_SIZE * 5, y: TILE_SIZE * 7 },
+    { x: TILE_SIZE * 7, y: TILE_SIZE * 7 },
+    { x: TILE_SIZE * 9, y: TILE_SIZE * 7 },
+    { x: TILE_SIZE * 11, y: TILE_SIZE * 7 },
+
+    { x: TILE_SIZE * 1, y: TILE_SIZE * 9 },
+    { x: TILE_SIZE * 3, y: TILE_SIZE * 9 },
+    { x: TILE_SIZE * 5, y: TILE_SIZE * 9 },
+    { x: TILE_SIZE * 7, y: TILE_SIZE * 9 },
+    { x: TILE_SIZE * 9, y: TILE_SIZE * 9 },
+    { x: TILE_SIZE * 11, y: TILE_SIZE * 9 },
+
+    { x: TILE_SIZE * 1, y: TILE_SIZE * 11 },
+    { x: TILE_SIZE * 3, y: TILE_SIZE * 11 },
+    { x: TILE_SIZE * 5, y: TILE_SIZE * 11 },
+    { x: TILE_SIZE * 7, y: TILE_SIZE * 11 },
+    { x: TILE_SIZE * 9, y: TILE_SIZE * 11 },
+    { x: TILE_SIZE * 11, y: TILE_SIZE * 11 }
+];
