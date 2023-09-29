965c757ca76e355a963c3f109bf1b5428501f582
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2023-09-29 11:40:58 +0200

    feat: add bomb in game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,61 +1,154 @@
 import { Sprite, Stage, TilingSprite } from "@pixi/react";
 import { useEffect, useMemo, useState } from "react";
 
-import { MAP_SIZE, TILE_SIZE, WALLS } from "../constants";
+import { MAP_SIZE, TILE_SIZE, WALLS, BOMB_DELAY } from "../constants";
 import Wall from "./Wall";
-
-function checkCollision(coords: { x: number, y: number }) {
-    return WALLS.some((wall) => wall.x === coords.x && wall.y === coords.y);
-}
+import Bomb from "./Bomb";
+import Explosion from "./Explosion";
 
 function Game() {
     // Position du joueur
     const [coords, setCoords] = useState({ x: 0, y: 0 });
+    const [bombs, setBombs] = useState([]);
+    const [explosions, setExplosions] = useState([]);
+    const [gameOver, setGameOver] = useState(false);
+
     const walls = useMemo(() => WALLS, []);
+
+    const checkCollision = (coords: { x: number; y: number }) => {
+        return (
+            walls.some((wall) => wall.x === coords.x && wall.y === coords.y) ||
+            bombs.some((bomb) => bomb.x === coords.x && bomb.y === coords.y)
+        );
+    };
+
+    // TODO : check bombs collision
+    const moveUp = () => {
+        setCoords((coords) => {
+            return { ...coords, y: coords.y - TILE_SIZE };
+        });
+    };
+
+    const moveDown = () => {
+        setCoords((coords) => {
+            return { ...coords, y: coords.y + TILE_SIZE };
+        });
+    };
+
+    const moveLeft = () => {
+        setCoords((coords) => {
+            return { ...coords, x: coords.x - TILE_SIZE };
+        });
+    };
+
+    const moveRight = () => {
+        setCoords((coords) => {
+            return { ...coords, x: coords.x + TILE_SIZE };
+        });
+    };
 
     // Gère les mouvements du joueurs
     useEffect(() => {
-        window.addEventListener("keydown", (e) => {
+        const handleMove = (e) => {
             if (e.key === "ArrowUp") {
-                setCoords((coords) => {
-                    const nextTile = { ...coords, y: coords.y - TILE_SIZE };
-                    if (checkCollision(nextTile)) return coords;
-                    if (coords.y > 0) return { ...coords, y: coords.y - (TILE_SIZE / 2) };
-                    return coords;
-                });
+                const nextCoords = { ...coords, y: coords.y - TILE_SIZE };
+                if (nextCoords.y < 0 || checkCollision(nextCoords)) return;
+                moveUp();
             }
             if (e.key === "ArrowDown") {
-                setCoords((coords) => {
-                    const nextTile = { ...coords, y: coords.y + TILE_SIZE };
-                    if (checkCollision(nextTile)) return coords;
-                    if (coords.y < MAP_SIZE - TILE_SIZE) return { ...coords, y: coords.y + (TILE_SIZE / 2) };
-                    return coords;
-                });
+                const nextCoords = { ...coords, y: coords.y + TILE_SIZE };
+                if (
+                    nextCoords.y > MAP_SIZE - TILE_SIZE ||
+                    checkCollision(nextCoords)
+                )
+                    return;
+                moveDown();
             }
             if (e.key === "ArrowLeft") {
-                setCoords((coords) => {
-                    const nextTile = { ...coords, x: coords.x - TILE_SIZE };
-                    if (checkCollision(nextTile)) return coords;
-                    if (coords.x > 0) return { ...coords, x: coords.x - (TILE_SIZE / 2) };
-                    return coords;
-                });
+                const nextCoords = { ...coords, x: coords.x - TILE_SIZE };
+                if (nextCoords.x < 0 || checkCollision(nextCoords)) return;
+                moveLeft();
             }
             if (e.key === "ArrowRight") {
-                setCoords((coords) => {
-                    const nextTile = { ...coords, x: coords.x + TILE_SIZE };
-                    if (checkCollision(nextTile)) return coords;
-                    if (coords.x < MAP_SIZE - TILE_SIZE) return { ...coords, x: coords.x + (TILE_SIZE / 2) };
-                    return coords;
-                });
+                const nextCoords = { ...coords, x: coords.x + TILE_SIZE };
+                if (
+                    nextCoords.x > MAP_SIZE - TILE_SIZE ||
+                    checkCollision(nextCoords)
+                )
+                    return;
+                moveRight();
             }
-        });
-    }, []);
+        };
+
+        const handlePlantBomb = (e) => {
+            if (e.key !== " ") return;
+
+            setBombs((bombs) => [...bombs, { ...coords }]);
+
+            setTimeout(() => {
+                setBombs((bombs) =>
+                    bombs.filter(
+                        ({ x, y }) => !(x === coords.x && y === coords.y)
+                    )
+                );
+            }, BOMB_DELAY);
+        };
+
+        window.addEventListener("keydown", handleMove);
+        window.addEventListener("keypress", handlePlantBomb);
+
+        return () => {
+            window.removeEventListener("keypress", handlePlantBomb);
+            window.removeEventListener("keydown", handleMove);
+        };
+    }, [coords, coords.x, coords.y, bombs]);
+
+    useEffect(() => {
+        if (bombs.length === 0) return;
+
+        const zones = [
+            { x: coords.x, y: coords.y },
+            { x: coords.x + TILE_SIZE, y: coords.y },
+            { x: coords.x - TILE_SIZE, y: coords.y },
+            { x: coords.x, y: coords.y + TILE_SIZE },
+            { x: coords.x, y: coords.y - TILE_SIZE }
+        ];
+
+        setTimeout(() => {
+            setExplosions((_explosions) => { 
+                // if player is in explosion zone
+                if (zones.some(({ x, y }) => x === coords.x && y === coords.y)) {
+                    console.log("dead");
+                }
+
+                return [..._explosions, ...zones];
+            });
+        }, BOMB_DELAY);
+
+        // clear explosions
+        setTimeout(() => {
+            setExplosions((_explosions) => {
+                return _explosions.filter(
+                    ({ x, y }) =>
+                        !zones.some(
+                            (zone) => zone.x === x && zone.y === y
+                        )
+                );
+            });
+        }, BOMB_DELAY * 1.5);
+    }, [bombs]);
+
+    if (gameOver) {
+        return (
+            <div>
+                <h1>Game Over</h1>
+                <button onClick={() => setGameOver(false)}>Rejouer</button>
+            </div>
+        )
+    }
 
     return (
-        <Stage
-            width={MAP_SIZE} 
-            height={MAP_SIZE}
-        >
+        <Stage width={MAP_SIZE} height={MAP_SIZE}>
             {/* Grid */}
             <TilingSprite
                 image={"/src/assets/damier.png"}
@@ -65,13 +158,21 @@ function Game() {
                 tileScale={{ x: 1, y: 1 }}
             />
 
+            {bombs.map((bomb, index) => (
+                <Bomb key={index} x={bomb.x} y={bomb.y} />
+            ))}
+
+            {explosions.map((explosion, index) => (
+                <Explosion key={index} x={explosion.x} y={explosion.y} />
+            ))}
+
             {walls.map((wall, index) => (
                 <Wall key={index} x={wall.x} y={wall.y} />
             ))}
 
             {/* Player */}
             <Sprite
-                image="https://pixijs.io/pixi-react/img/bunny.png"
+                image={"/src/assets/Monkey_Front_1.png"}
                 x={coords.x}
                 y={coords.y}
                 width={TILE_SIZE}
