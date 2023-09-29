7692e486202705622bc67cf0007452468313a07d
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2023-09-29 11:40:31 +0200

    feat: add bomb and explosion components

---
+++ src/components/Bomb.tsx
@@ -0,0 +1,17 @@
+import { Sprite } from "@pixi/react";
+import { TILE_SIZE } from "../constants";
+
+function Bomb({x, y}: { x: number, y: number }) {
+    return (
+        <Sprite
+            image={"/src/assets/banana.png"}
+            x={x}
+            y={y}
+            width={TILE_SIZE}
+            height={TILE_SIZE}
+            anchor={{ x: 0, y: 0 }}
+        />
+    );
+}
+
+export default Bomb;
