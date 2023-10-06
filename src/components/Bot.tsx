5b90a2197b24832e4800b73f1ee73c337745bcc9
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2023-10-06 10:01:52 +0200

    feat: add bot component

---
+++ src/components/Bot.tsx
@@ -0,0 +1,17 @@
+import { Sprite } from "@pixi/react";
+import { TILE_SIZE } from "../constants";
+
+function Bot({ x, y }: { x: number; y: number }) {
+    return (
+        <Sprite
+            image={"/src/assets/face_bot.png"}
+            x={x}
+            y={y}
+            width={TILE_SIZE}
+            height={TILE_SIZE}
+            anchor={{ x: 0, y: 0 }}
+        />
+    );
+}
+
+export default Bot;
