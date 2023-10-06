e52f78ffe98b608e59bc01dff216a7fdddb20190
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2023-10-06 10:02:30 +0200

    feat: add player component

---
+++ src/components/Player.tsx
@@ -0,0 +1,17 @@
+import { Sprite } from "@pixi/react";
+import { TILE_SIZE } from "../constants";
+
+function Player({ x, y }: { x: number; y: number }) {
+    return (
+        <Sprite
+            image={"/src/assets/Singe_1.png"}
+            x={x}
+            y={y}
+            width={TILE_SIZE}
+            height={TILE_SIZE}
+            anchor={{ x: 0, y: 0 }}
+        />
+    );
+}
+
+export default Player;
